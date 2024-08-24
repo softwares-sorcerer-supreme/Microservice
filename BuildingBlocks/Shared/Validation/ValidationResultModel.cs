39f20affab3c357b8374d3e4ff915bd75ad3b14a
Hoang Manh Hai <haihoang2907@gmail.com>
2024-08-25 03:15:39 +0700

    change dockerfile and initialize ApiGateway using Ocelot

--- BuildingBlocks/Shared/Validation/ValidationResultModel.cs
+++ BuildingBlocks/Shared/Validation/ValidationResultModel.cs
@@ -7,7 +7,6 @@ namespace Shared.Validation;
 public class ValidationResultModel
 {
     public int StatusCode { get; set; } = (int)HttpStatusCode.BadRequest;
-    public string Message { get; set; } = "Validation Failed";
     public List<ValidationError> Errors { get; set; }
 
     public override string ToString()
